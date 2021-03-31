009e39c492d4a8b70439e83847c370f9ce76090e
Hengguang Zhou <743238614@qq.com>
2021-03-31 12:16:24 +0800

    第五次作业第一版

---
+++ Homework5/OrderUtils/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OrderUtils
+{
+    public class Order
+    {
+        public Order(int id, String goodsName, int goodsPrice, DateTime time, String fromName, int fromid, String toName, int toid)
+        {
+            this.id = id;
+            this.detail = new OrderDetails(goodsName, goodsPrice);
+            this.time = time;
+            this.from = new Customer(fromName, fromid);
+            this.to = new Customer(toName, toid);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Order comparedOrder = obj as Order;
+            return comparedOrder != null && comparedOrder.ID != id;
+        }
+
+        public override string ToString()
+        {
+            return $"ID:{id}; GoodsName:{GoodsName}; Price:{Price}; CustomerName:{CustomerName}";
+        }
+
+        public int ID => id;
+        public String GoodsName => detail.GoodsName;
+        public int Price => detail.GoodsPrice;
+        public String CustomerName => to.Name;
+        private int id;
+        private OrderDetails detail;
+        private DateTime time;
+        private Customer from;
+        private Customer to;
+    }
+}
---
+++ Homework5/OrderUtils/OrderDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OrderUtils
+{
+    public class OrderDetails
+    {
+        public OrderDetails(String goodsName, int goodsPrice)
+        {
+            this.goodsName = goodsName;
+            this.goodsPrice = goodsPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"GoodsName:{GoodsName}; GoodsPrice:{goodsPrice}";
+        }
+
+        public string GoodsName => goodsName;
+
+        public int GoodsPrice => goodsPrice;
+
+        private String goodsName;
+        private int goodsPrice;
+    }
+}
